23ea486c2b8385cac0d7bdbc449fc22e90102009
akella <akella.a@gmail.com>
2015-09-16 15:33:37 +0300

    initial commit

---
+++ js/common.js
@@ -0,0 +1,19 @@
+head.ready(function() {
+
+	// $(document).on("click", function(){
+	// 	$(".js-popup").hide();
+	// });
+
+	// function scrollFixedElements() {
+	//     var scroll_left = $(this).scrollLeft();
+	//     $(".fixed-element").css({
+	//         left: -scroll_left
+	//     });
+	// }
+	// scrollFixedElements();
+	// $(window).scroll(function(){
+	//     scrollFixedElements()
+	// });
+
+	console.log($('body').html());
+});
---
+++ js/init.js
@@ -0,0 +1,3 @@
+head.load("js/lib/jquery.js", 
+					"js/lib/cycle.js",
+					"js/common.js");
