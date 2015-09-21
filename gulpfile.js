673ba0d1bfd1a92b9fa19717e6514e7bfa026e1f
akella <akella.a@gmail.com>
2015-09-21 09:45:56 +0300

    test svg iconfont

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,10 @@ var gulp = require('gulp'),
     rigger = require('gulp-rigger'),
     spritesmith = require('gulp.spritesmith'),
     browserSync = require("browser-sync"),
+    rimraf = require('rimraf'),
     reload = browserSync.reload;
 
 // @TODO: add rimraf to cleanup old files
-// @TODO: add icon font creation
-// @TODO: add jade task
 // @TODO: move all paths to these variables
 var src = {
     root    : 'src',
@@ -64,7 +63,7 @@ gulp.task('sprite', function() {
         cssName: '_sprite.sass',
         imgPath: '../img/icons.png',
         cssFormat: 'sass',
-        padding: 10,
+        padding: 4,
         // algorithm: 'top-down',
         cssTemplate: src.helpers + '/sprite.template.mustache'
     }));
@@ -89,10 +88,85 @@ gulp.task('js', function () {
         .pipe(gulp.dest('site/js/'))
         .pipe(reload({stream: true}));
 });
+
 gulp.task('copy', function() {
    gulp.src('src/img/*.*')
    .pipe(gulp.dest('site/img/')); 
 });
+
+gulp.task('clean', function (cb) {
+    rimraf('./site', cb);
+});
+
+
+
+// jade, requires:
+// = gulp-jade
+// = gulp-changed
+// = gulp-plumber
+// gulp.task('jade', function() {
+//     return gulp.src([src.jade + '/*.jade', '!' + src.jade + '/_*.jade', '!' + src.jade + '/includes/*.jade'])
+//         .pipe(plumber({errorHandler: notify.onError(function(error){return error.message;})}))
+//         .pipe(changed(dest.html, {extension: '.html'}))
+//         .pipe(jade({pretty: true}))
+//         .pipe(gulp.dest(dest.html));
+// });
+
+// //compile all jade files
+// gulp.task('jade-all', function() {
+//     return gulp.src([src.jade + '/*.jade', '!' + src.jade + '/_*.jade', '!' + src.jade + '/includes/*.jade'])
+//         .pipe(plumber({errorHandler: notify.onError(function(error){return error.message;})}))
+//         .pipe(jade({pretty: true}))
+//         .pipe(gulp.dest(dest.html));
+// });
+
+
+// icon font, requires:
+// = gulp-iconfont
+// = gulp-consolidate
+// = gulp-lodash
+// = gulp-svgmin
+// var fontName = 'svgfont';
+// gulp.task('font', function(){
+//   return gulp.src('src/img/svg/*.svg')
+//     .pipe(svgmin())
+//     .pipe(iconfont({
+//       fontName: 'svgfont',
+//       appendUnicode: true,
+//       formats: ['ttf', 'eot', 'woff', 'woff2'],
+//       // timestamp: runTimestamp,
+//       normalize: true,
+//       fontHeight: 1001,
+//       fontStyle: 'normal',
+//       fontWeight: 'normal'
+//     }))
+//     .on('glyphs', function(glyphs, options) {
+//         console.log(glyphs);
+//         gulp.src('src/sass/_svgfont.scss')
+//             .pipe(consolidate('lodash', {
+//                 glyphs: glyphs,
+//                 fontName: 'svgfont',
+//                 fontPath: '../css/fonts/',
+//                 className: 'icon'
+//             }))
+//             .pipe(gulp.dest('src/sass/'));
+//         gulp.src('src/assets/icons.html')
+//             .pipe(consolidate('lodash', {
+//                 glyphs: glyphs,
+//                 fontName: 'svgfont',
+//                 fontPath: '../css/fonts/',
+//                 className: 'icon',
+//                 htmlBefore: '<i class="icon ',
+//                 htmlAfter: '"></i>',
+//                 htmlBr: '<br>'
+//             }))
+//             .pipe(gulp.dest('build/'));
+//     })
+//     .pipe(gulp.dest('site/css/fonts/'))
+//     .pipe(reload({stream: true}));
+// });
+
+
 
 
 //webserver
