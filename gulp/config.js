cb7948ed7faf99105592b3f459e94fa6f91609e7
akella <akella.a@gmail.com>
2015-12-05 14:40:48 +0200

    refactor tasks

---
+++ gulp/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  src : {
+    root    : 'src/',
+    jade    : 'src/jade',
+    sass    : 'src/sass/',
+    js      : 'src/js/',
+    img     : 'src/img/',
+    helpers : 'src/helpers/'
+  },
+  dest:{
+    root    : 'site/',
+    css     : 'site/css/',
+    html    : 'site/',
+    js      : 'site/js/',
+    img     : 'site/img/'
+  },
+};
