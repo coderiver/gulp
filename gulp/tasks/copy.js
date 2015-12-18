2e62e8b7c3717e753b370e8d67abd089a49411d4
Eugene Godun <eugene.godun@gmail.com>
2015-12-18 12:40:11 +0700

    copy whole image folder folder and exclude img/icons/ and img/svg/

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var config = require('../config');
-
+var imgIcons = config.src.img+'icons/*.*';
+var imgSvg = config.src.img+'svg/*.*';
 // copy static files
 gulp.task('copy', function() {
-   gulp.src(config.src.img+'*.*')
+   gulp.src([config.src.img+'**/*.*', '!'+imgIcons, '!'+imgSvg])
    .pipe(gulp.dest(config.dest.img));
    gulp.src(config.src.root+'fonts/*.*')
    .pipe(gulp.dest(config.dest.css+'fonts/'));
