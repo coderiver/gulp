4d15e3cc70da69b00c98633162100a3b8cbb9dcf
eugenegodun <eugene.godun@gmail.com>
2016-03-14 21:11:21 +0700

    gulp-uglify

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var include = require("gulp-include");
+var uglify = require('gulp-uglify');
 var config = require('../config');
 var browserSync = require('browser-sync');
 reload = browserSync.reload;
@@ -8,6 +9,7 @@ gulp.task('js', function () {
     gulp.src(config.src.js+'**/*.js')
         .pipe(include())
         .on('error', function(){notify("Javascript include error");})
+        //.pipe(uglify())
         .pipe(gulp.dest(config.dest.js))
         .pipe(reload({stream: true}));
 });
